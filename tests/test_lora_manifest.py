6c969398b7d2e7d708aa5cb203bdf68a19d5ce06
grawthings-beep <i1t1c0h7k@gmail.com>
2026-06-30 04:46:11 +0900

    Add Marciana Anima LoRA manifest entry

--- tests/test_lora_manifest.py
+++ tests/test_lora_manifest.py
@@ -37,8 +37,14 @@ class LoraManifestTests(unittest.TestCase):
         by_id = {entry["id"]: entry for entry in self.entries}
         self.assertEqual(by_id["label"]["trigger"], "l4bel")
         self.assertEqual(by_id["arkrangerblack"]["trigger"], "4rkblack")
+        self.assertEqual(by_id["marciana"]["trigger"], "m4rciana")
         self.assertEqual(by_id["anisstar3"]["trigger"], "an1sstar3")
         self.assertEqual(by_id["pixel-came"]["trigger"], "CAME")
+        self.assertEqual(
+            by_id["marciana"]["url"],
+            "https://huggingface.co/uwgm/nikke-loras/resolve/main/"
+            "anima_marciana.safetensors",
+        )
         self.assertEqual(
             by_id["anisstar3"]["url"],
             "https://huggingface.co/uwgm/nikke-loras/resolve/main/"
